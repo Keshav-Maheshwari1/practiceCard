f0bcea501ca0758b72feecf84fbbd4687aa91bc4
Keshav <maheshwari.keshav2090@gmail.com>
2024-09-23 12:43:39 +0530

    created

--- src/components/PlayCard.tsx
+++ src/components/PlayCard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 
 const PlayCard: React.FC = () => {
   const [toggle, setToggle] = useState<boolean>(false);
